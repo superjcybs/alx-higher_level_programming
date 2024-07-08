e1d1aad94d82a4f69218c267a87e195368ccb673
Superjcybs <jerrybabatunde1@gmail.com>
2024-07-07 18:32:14 -0700

    program that imports the variable a from the file variable_load_5.py and prints its value

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from variable_load_5 import a
+
+def main():
+    """
+    Print the value of variable 'a' from variable_load_5
+    """
+    print(a)
+
+if __name__ == "__main__":
+    main()
+
